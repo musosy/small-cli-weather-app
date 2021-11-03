20943d6aac48f47a946ec7f86de1e6343c0d232f
musosy <hugo.guillaume13@gmail.com>
2021-11-03 16:47:17 +0100

    refacto

--- cmds/forecast.js
+++ cmds/forecast.js
@@ -7,8 +7,6 @@ module.exports = async args => {
         const location = args.location || args.l || await ipLocation();
         const weather = await forecast(location);
         console.log(`Forecast for the next 7 days in ${weather.city}:`)
-        console.log('')
-        console.log('')
         let table = []
         for (let i = 0; i < weather.daily.time.length; i++) {
             table.push({
@@ -16,7 +14,7 @@ module.exports = async args => {
                 "Weather": weatherInterpretationCodes[weather.daily.weathercode[i]],
                 "Min temp ( °C)": weather.daily.apparent_temperature_min[i], 
                 "Max temp ( °C)": weather.daily.apparent_temperature_max[i],
-                "Precipitation time": weather.daily.precipitation_hours[i]
+                "Prec time": weather.daily.precipitation_hours[i]
             })
         }
         console.table(table)
