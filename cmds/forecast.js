6f0056e90d06ba4989f92d23ae58f071cb564200
musosy <hugo.guillaume13@gmail.com>
2021-11-04 10:44:17 +0100

    added some chalk for coloring, a spinner and a table utils to color the table

--- cmds/forecast.js
+++ cmds/forecast.js
@@ -1,24 +1,40 @@
+const chalk = require('chalk');
+const Spinner = require('clui').Spinner;
+const { Table } = require('console-table-printer')
 const { forecast } = require('../utils/weather');
 const weatherInterpretationCodes = require('../utils/weatherInterpretationCodes');
 const ipLocation = require('../utils/location')
 
 module.exports = async args => {
+    let wait = new Spinner('Loading forecast ...');
+    wait.start();
     try {
         const location = args.location || args.l || await ipLocation();
         const weather = await forecast(location);
-        console.log(`Forecast for the next 7 days in ${weather.city}:`)
-        let table = []
+        
+        let table = new Table({
+            columns: [
+                {name: "date", alignment: "center", title: "Date (Y-M-D)"},
+                {name: "weather", alignment: "center", title: "Weather"},
+                {name: "min", title: "Min temp ( °C)"},
+                {name: "max", title: "Max temp ( °C)"},
+                {name: "prec", title: "Precipitation time"}
+            ]
+        });
         for (let i = 0; i < weather.daily.time.length; i++) {
-            table.push({
-                "Date": weather.daily.time[i],
-                "Weather": weatherInterpretationCodes[weather.daily.weathercode[i]],
-                "Min temp ( °C)": weather.daily.apparent_temperature_min[i], 
-                "Max temp ( °C)": weather.daily.apparent_temperature_max[i],
-                "Prec time": weather.daily.precipitation_hours[i]
+            table.addRow({
+                date: new Date(weather.daily.time[i]).toDateString(),
+                weather: weatherInterpretationCodes[weather.daily.weathercode[i]],
+                min: chalk.blue(weather.daily.apparent_temperature_min[i]), 
+                max: chalk.red(weather.daily.apparent_temperature_max[i]),
+                prec: chalk.green(weather.daily.precipitation_hours[i])
             })
         }
-        console.table(table)
+        wait.stop()
+        console.log(`Forecast for the next 7 days in ${chalk.bold.blue(weather.city)}:`)
+        table.printTable()
     } catch (err) {
+        wait.stop();
         console.log(err)
     }
 }
--- cmds/today.js
+++ cmds/today.js
@@ -1,14 +1,21 @@
+const chalk = require('chalk')
+const Spinner = require('clui').Spinner;
 const { today } = require('../utils/weather');
 const weatherInterpretationCodes = require('../utils/weatherInterpretationCodes');
 const ipLocation = require('../utils/location')
 
+
 module.exports = async args => {
+    let wait = new Spinner('Loading weather information ...')
+    wait.start()
     try {
         let location = args.location || args.l || await ipLocation();
         const weather = await today(location);
-        console.log(`Current conditions in ${weather.city}:`)
-        console.log(`${weather.current_weather.temperature} °C and ${weatherInterpretationCodes[weather.current_weather.weathercode].toLowerCase()}`)
+        wait.stop()
+        console.log(`Current conditions in ${chalk.bold.blue(weather.city)}:`)
+        console.log(`${chalk.green(weather.current_weather.temperature + " °C")}  and ${weatherInterpretationCodes[weather.current_weather.weathercode].toLowerCase()}`)
     } catch (err) {
+        wait.stop()
         console.log(err)
     }
 }
